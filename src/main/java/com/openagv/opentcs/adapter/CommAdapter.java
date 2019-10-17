4e23458d3fc12f446745e2f24162009881dfd13b
tcrct@163.com <790731tcrct>
2019-10-17 13:55:45 +0800

    待续开发

--- src/main/java/com/openagv/opentcs/adapter/CommAdapter.java
+++ src/main/java/com/openagv/opentcs/adapter/CommAdapter.java
@@ -232,8 +232,8 @@ public class CommAdapter extends BasicVehicleCommAdapter {
     protected void connectVehicle() {
         logger.info("###########connectVehicle");
         // TODO 可以改为下拉选择的方式 ，待完成，目前先将起点位置设置为Point-0001
-        getProcessModel().setVehiclePosition("36");
-//        getProcessModel().setVehiclePosition("1");
+//        getProcessModel().setVehiclePosition("36");
+        getProcessModel().setVehiclePosition("1");
 //        getProcessModel().setVehiclePosition("705");
         getProcessModel().setVehicleState(Vehicle.State.IDLE);
         getProcessModel().setVehicleIdle(true);
