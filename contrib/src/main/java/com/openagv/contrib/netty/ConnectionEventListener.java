83cbd56eaa45fab56d51e71cd730ead64a713d8b
tcrct@163.com <tcrct790731>
2020-01-14 23:33:28 +0800

    待续开发

--- contrib/src/main/java/com/openagv/contrib/netty/ConnectionEventListener.java
+++ contrib/src/main/java/com/openagv/contrib/netty/ConnectionEventListener.java
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) The openTCS Authors.
- *
- * This program is free software and subject to the MIT license. (For details,
- * see the licensing information (LICENSE.txt) you should have received with
- * this copy of the software.)
- */
 package com.openagv.contrib.netty;
 
 /**
