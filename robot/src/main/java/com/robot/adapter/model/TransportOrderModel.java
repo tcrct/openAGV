f7ebfc60cba4bcc792b11275458e27535621363b
tcrct <tcrct@163.com>
2020-03-20 08:37:07 +0800

    待续开发

--- robot/src/main/java/com/robot/adapter/model/TransportOrderModel.java
+++ robot/src/main/java/com/robot/adapter/model/TransportOrderModel.java
@@ -1,11 +1,14 @@
 package com.robot.adapter.model;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.IdUtil;
 import com.robot.adapter.constants.RobotConstants;
 import com.robot.utils.ElementKit;
 import com.robot.utils.RobotUtil;
 import org.opentcs.data.order.Route;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -14,6 +17,11 @@ import java.util.Queue;
  * @author Laotang
  */
 public class TransportOrderModel implements java.io.Serializable {
+
+    /**
+     * 该对象ID，唯一，要与订单ID区分
+     */
+    private String id;
 
     /**
      * 订单ID
@@ -38,11 +46,21 @@ public class TransportOrderModel implements java.io.Serializable {
 
     private Queue<Route.Step> routeStep;
 
-    private TransportOrderModel(String vehicleName, String finalPosition, String finalLocation, String finalOperation) {
+    private List<String> pointPaths;
+    private String pointPathStr;
+
+    private TransportOrderModel(String vehicleName, String finalPosition, String finalLocation, String finalOperation, List<String> pointPaths) {
+        this.id = IdUtil.objectId();
         this.vehicleName = vehicleName;
         this.finalPosition = finalPosition;
         this.finalLocation = finalLocation;
         this.finalOperation = finalOperation;
+        this.pointPaths = pointPaths;
+        this.pointPathStr = CollectionUtil.join(pointPaths.iterator(), ",");
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getFinalPosition() {
@@ -69,11 +87,20 @@ public class TransportOrderModel implements java.io.Serializable {
         this.orderId = orderId;
     }
 
+    public List<String> getPointPathList() {
+        return pointPaths;
+    }
+
+    public String getPointPathStr() {
+        return pointPathStr;
+    }
+
     public static class Builder {
         private String vehicleName;
         private String finalPosition;
         private String finalLocation;
         private String finalOperation;
+        private List<String> pointPaths;
 
         public Builder() {}
 
@@ -100,6 +127,11 @@ public class TransportOrderModel implements java.io.Serializable {
             return this;
         }
 
+        public Builder pointPaths(List<String> pointPaths) {
+            this.pointPaths = pointPaths;
+            return this;
+        }
+
 
         private void setFinalOperation(String finalPosition) {
             try {
@@ -115,7 +147,7 @@ public class TransportOrderModel implements java.io.Serializable {
 
         public TransportOrderModel build() {
             setFinalOperation(finalPosition);
-            return new TransportOrderModel(vehicleName, finalPosition, finalLocation, finalOperation);
+            return new TransportOrderModel(vehicleName, finalPosition, finalLocation, finalOperation, pointPaths);
         }
     }
 
